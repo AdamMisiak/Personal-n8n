f6715e3b7166ad3685e41f8fa47e21daf2ea9af6
Adam Misiak <adammisiak3@gmail.com>
2025-07-09 20:21:47 +0200

    Update init-letsencrypt script for testing mode and adjust Nginx configuration for HTTP/2 support

--- docker-compose/withPostgresAndWorker/init-letsencrypt.sh
+++ docker-compose/withPostgresAndWorker/init-letsencrypt.sh
@@ -11,7 +11,7 @@ domains="$N8N_DOMAIN"
 rsa_key_size=4096
 data_path="./certbot"
 email="${LETSENCRYPT_EMAIL}" # Email from environment variable
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
